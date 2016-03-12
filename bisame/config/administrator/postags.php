e966fca5dc3b5b971880ecef309f54b2a9973148
Alice <alice.millour@abtela.eu>
2016-03-13 00:13:16 +0100

    add postags

---
+++ bisame/config/administrator/postags.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'title' => 'Postags',
+    'single' => 'postag',
+    'model' => 'App\Models\Postag',
+    'columns' => array(
+        'name',
+    ),
+    'edit_fields' => array(
+        'name',
+    )
+);
