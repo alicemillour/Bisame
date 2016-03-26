f0ce95e77d6802a8f3f54d23edc2b57393145cfa
Emilie Paillous <emilie.paillous@gmail.com>
2016-03-26 17:13:21 +0100

    stuck array of category on right

--- bisame/resources/views/games/edit.blade.php
+++ bisame/resources/views/games/edit.blade.php
@@ -7,42 +7,42 @@
 @endsection
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <article class="row bg-primary">
-			<div class="col-md-12">
-				<header>
-					<h1>Annotez ces mots
-						<div class="pull-right">
-						</div>
-					</h1>
-				</header>
-				<hr>
-				<div class="sentence-container">
-					@foreach($sentence->words as $word)
-						<div class="word-container">
-							<div class="word">{{ $word->value }}</div>
-							<div class="category"> </div>
-						</div>
-					@endforeach
-				</div>
+    <div class="row sentence-main-container">
+        <article class="row bg-primary">
+		<div class="col-md-12">
+			<header>
+				<h1>Annotez ces mots
+					<div class="pull-right">
+					</div>
+				</h1>
+			</header>
+			<hr>
+			<div class="sentence-container">
+				@foreach($sentence->words as $word)
+					<div class="word-container">
+						<div class="word">{{ $word->value }}</div>
+						<div class="category"> </div>
+					</div>
+				@endforeach
 			</div>
+		</div>
 		</article>
-        </div>
     </div>
-    <table class="table table-hover categories-table" hidden="true">
-    	<head>
-    		<tr>
-    			<th>Categorie</th>
-    		</tr>
-    	</head>
-    	<tbody>
-    		@foreach($postags as $postag)
-    		<tr>
-    			<td>{{ $postag->name }}</td>
-    		</tr>
-    		@endforeach
-    	</tbody>
-    </table>
+    <div class ="categorie-table-container">
+	    <table class="table table-hover categories-table" hidden="true">
+	    	<head>
+	    		<tr>
+	    			<th>Categorie</th>
+	    		</tr>
+	    	</head>
+	    	<tbody>
+	    		@foreach($postags as $postag)
+	    		<tr>
+	    			<td>{{ $postag->name }}</td>
+	    		</tr>
+	    		@endforeach
+	    	</tbody>
+	    </table>
+	</div>
 </div>
 @endsection
