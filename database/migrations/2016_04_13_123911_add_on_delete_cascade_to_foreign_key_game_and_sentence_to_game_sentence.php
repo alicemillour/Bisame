5dbef24527401c2716b163d19a487e0fc37ff20d
Alice <alice.millour@abtela.eu>
2016-04-13 15:05:51 +0200

    add migration files for add on delete cascade on foreign keys on tables game and sentence game

---
+++ database/migrations/2016_04_13_123911_add_on_delete_cascade_to_foreign_key_game_and_sentence_to_game_sentence.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOnDeleteCascadeToForeignKeyGameAndSentenceToGameSentence extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::table('game_sentence', function ($table) {
+            $table->dropForeign('game_sentence_game_id_foreign');
+            $table->foreign('game_id')->references('id')->on('games')
+                ->onDelete('cascade');
+            $table->dropForeign('game_sentence_sentence_id_foreign');
+            $table->foreign('sentence_id')->references('id')->on('sentences')
+                ->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    Schema::table('game_sentence', function ($table) {
+            $table->dropForeign('game_sentence_game_id_foreign');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->dropForeign('game_sentence_sentence_id_foreign');
+            $table->foreign('sentence_id')->references('id')->on('sentences');
+    });
+    }
+}
