e705771a74c41b433f8a20e446c2f63658f518da
Alice <alice.millour@abtela.eu>
2016-04-16 18:39:37 +0200

    Return count word not punct in trainig to check everything_is_correct

--- app/Repositories/SentenceRepository.php
+++ app/Repositories/SentenceRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\Sentence;
+use App\Models\Word;
+use DB;
 
 class SentenceRepository extends ResourceRepository
 {
@@ -10,13 +12,21 @@ class SentenceRepository extends ResourceRepository
     //protected $user;
 
     public function __construct(Sentence $sentence)
-	{
+    {
 		$this->sentence = $sentence;
-	}
+    }
 
-	public function all()
-	{
-		return $this->sentence->all();
-	}
+    public function all()
+    {
+            return $this->sentence->all();
+    }
+        
+    public function getWordNotPunctCount($sentence_id)
+    {
+        return Sentence::join('words','sentence_id','=','sentences.id')
+                ->select(DB::raw('count(*) as word_not_punct_count'))
+                ->where('sentence_id',$sentence_id)
+                ->whereRaw("value NOT REGEXP '[[:punct:]]'")->first();
+    }    
 
 }
