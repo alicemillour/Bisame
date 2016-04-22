de226b70986eda53f6ff77e3bc6189fc7e521865
Alice <alice.millour@abtela.eu>
2016-04-22 16:42:52 +0200

    render error message with postag name

--- public/js/game.js
+++ public/js/game.js
@@ -49,94 +49,136 @@ $(document).ready(function(){
     });
 
     function reload_javascript_on_words() {
-        $('.word').hover(function() {
-            if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))){
+        $('.word').hover(function () {
+            if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))) {
                 $(this).addClass('highlighted');
             }
         },
-        function() {
-            if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))){
-                $(this).removeClass('highlighted');
-            }
-        });
-         $('.word').click(function() {
-            if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))){
+                function () {
+                    if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))) {
+                        $(this).removeClass('highlighted');
+                    }
+                });
+        $('.word').click(function () {
+            if (!/^[!"#$%&'()*+, \-./:;<=>?@ [\\\]^_`{|}~„“]$/.test($(this).attr('value'))) {
                 $('.word').removeClass('selected');
                 $(this).addClass('selected');
                 $('.sentence-main-container').width('60%');
                 get_words_postags($(this).attr('id'));
-        }
+            }
         });
-    };
+    }
+    ;
 
     function render_correction(answers) {
         postags_descriptions = [];
+        postags_full_names = [];
+        postags_names = [];
         error_status = ErrorLevel.error;
         for (var i = 0; i < answers.length; i++) {
-            if (answers[i]['is_correct'] == true) {
+            if (answers[i]['is_correct'] === true) {
                 $('#' + answers[i]['word_id'] + '.word').removeClass('is-in-error').addClass('is-correct');
                 error_status = ErrorLevel.warning;
             } else {
+                console.log(answers[i]['postag_description']);
                 $('#' + answers[i]['word_id'] + '.word').removeClass('is-correct').addClass('is-in-error');
-                postags_descriptions.push(answers[i]['postag_description']);
+                if (postags_names.indexOf(answers[i]['postag_name']) === -1){
+                    postags_descriptions.push(answers[i]['postag_description']);
+                    postags_full_names.push(answers[i]['postag_full_name']);
+                    postags_names.push(answers[i]['postag_name']);
+                }
             }
         }
-        if (postags_descriptions.length > 0) {error_status = ErrorLevel.error}
-        show_message(error_status, postags_descriptions);
+        if (postags_descriptions.length > 0) {
+            error_status = ErrorLevel.error;
+        }
+        show_message(error_status, postags_names, postags_full_names, postags_descriptions);
     }
-
+    
     function create_table_with_postags(postags) {
         var content = '';
-        for (var i = 0; i < postags.length; i++){
-            content += '<tr>';
-            content += '<td id=' + postags[i]['id'] + '>'+ postags[i]['name'] ;
-            content += '<span class=full-name-category> (' + postags[i]['full_name'] + ')</span>' ;
+        for (var i = 0; i < postags.length; i++) {
+            //content += '<tr id="popover" data-content="Content One" title="System Information">'
+            content += '<tr id="myPopover" data-placement="left" tabindex="50"  data-content="' + postags[i]['description'] + '" title="'+ postags[i]['description'] + '">';
+            //content += '<td>Utilities</td>'
+            content += '<td id=' + postags[i]['id'] + '>' + postags[i]['name'];
+            content += '<span class=full-name-category> (' + postags[i]['full_name'] +') ';
+            content += '</span>';
+            //content += '<img src="/images/question_mark.png" ';
+            //content += 'data-toggle="popover" data-placement="left" tabindex="50" </img></table>';
+            //content += 'title="' + postags[i]['name'] + '"';
+            //content += 'data-content="' + postags[i]['name'] + '"></img>';
             content += '</td>';
             content += '</tr>';
         }
         return content;
     }
-
-    function create_errors_content(postag_descriptions) {
+    
+    function create_table_with_descriptions(postags) {
         var content = '';
-        for (var i = 0; i < postag_descriptions.length; i++){
-            content += '<p>';
-            content += postag_descriptions[i];
-            content += '</p>';
+        for (var i = 0; i < postags.length; i++) {
+            content += '<tr id="myPopover">';
+            content += '<td id=' + postags[i]['id'] + '>' ;
+            content += '</td>';
+            content += '</tr>';
         }
         return content;
-    } 
+    }
+    
+    function create_errors_content(postag_names, postag_full_names, postag_descriptions) {
+        var content = '';
+        content += '<ul>';
+        for (var i = 0; i < postag_descriptions.length; i++) {
+            content += '<li><p>';
+            content += postag_names[i];
+            content += ' (<i>';
+            content += postag_full_names[i];
+            content += ' </i>) : ';
+            content += postag_descriptions[i];
+            content += '</p></li>';
+        }
+        content += '</ul>';
+        return content;
+    }
 
-    function show_message(is_in_error, postag_descriptions) {
-        switch(is_in_error) {
+    function show_message(is_in_error, postag_names, postag_full_names, postag_descriptions) {
+        switch (is_in_error) {
             case ErrorLevel.error:
-            $('#message-title').text("Erreur");
-            var content = create_errors_content(postag_descriptions);
-            $('#message-content').empty().append(content);
-            $('#message').removeClass('alert-success alert-warning').addClass('alert-danger');
-            break;
+                if (postag_names.length > 0) {
+                    $('#message-title').text("Vous avez des erreurs. Rappel sur les catégories que vous avez choisies :s");
+                } else {
+                    /* no word has been annotated yet */
+                    $('#message-title').text("Vous ne pouvez pas passer la phrase dans le mode entraînement");
+                    /* Je veux des messages différents en fonction du type de jeu 
+                    * $('#message-title').text("Vous devez annoter au moins un mot !");
+                    */
+                }
+                var content = create_errors_content(postag_names, postag_full_names, postag_descriptions);
+                $('#message-content').empty().append(content)
+                $('#message').removeClass('alert-success alert-warning').addClass('alert-danger');
+                break;
             case ErrorLevel.warning:
-            $('#message-title').text("Bravo !");
-            $('#message-content').text("Annotez les mots restant pour pouvoir passer à la suivante.");
-            $('#message').removeClass('alert-success alert-danger').addClass('alert-warning');
-            break;
+                $('#message-title').text("Bravo !");
+                $('#message-content').text("Annotez les mots restants pour pouvoir passer à la phrase suivante.");
+                $('#message').removeClass('alert-success alert-danger').addClass('alert-warning');
+                break;
             case ErrorLevel.success:
-            $('#message-title').text("Bravo !");
-            $('#message-content').text("Vous avez tout bon");
-            $('#message').removeClass('alert-danger alert-warning').addClass('alert-success');
-            break;
+                $('#message-title').text("Bravo !");
+                $('#message-content').text("Vous avez tout bon");
+                $('#message').removeClass('alert-danger alert-warning').addClass('alert-success');
+                break;
         }
         $('#message').show();
     }
     function get_words_postags(word_id) {
         $.ajax({
-            method : 'GET',
-            url : "/postags",
+            method: 'GET',
+            url: "/postags",
             dataType: 'json',
             data: {
                 word_id: word_id
             },
-            success : function(response){
+            success: function (response) {
                 console.log(response);
                 var content = create_table_with_postags(response['postags']);
                 $('.categories-table').find('tbody').empty().append(content);
