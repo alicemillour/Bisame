fb24f5ac1743d3bf062c583611d392e99ac0a576
Alice <alice.millour@abtela.eu>
2016-04-22 15:06:13 +0200

    change score column to float in user table

---
+++ database/migrations/2016_04_22_130208_change_score_type_to_float_in_user.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeScoreTypeToFloatInUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        DB::statement('ALTER TABLE users MODIFY COLUMN score float');
+    }
+
+    public function down()
+    {
+        DB::statement('ALTER TABLE users MODIFY COLUMN score int');
+    }   
+
+    }
