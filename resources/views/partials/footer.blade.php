45d492f9b4a6f35b88f6bb34f2059265c3a57158
Alice <alice.millour@abtela.eu>
2016-04-27 02:17:55 +0200

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+    <footer class="footer fina pull-right">
+      <div class="container">
+          <p class="text"><u>Contact</u> : Alice Millour - alice.millour@abtela.eu.</p>
+      </div>    
+    </footer>
