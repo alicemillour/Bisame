02e9ff506b3dd4971addd5ec9ed7d8fa08ed68f4
Alice <alice.millour@abtela.eu>
2016-07-28 19:48:04 +0200

    add navbar.js

---
+++ public/js/navbar.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function(){
+        console.log("ready navbar");
+   // getting the color before the color is changed ( not sure this is needed )
+  var thecolor = $('a.incognito').css("color");
+  $("a.incognito").mouseover(function() {
+    // setting the color previously picked
+    $(this).css({'color':thecolor});
+  });       
+});
