bebeae8a3da4c3e8da663c5d3927c64360f07907
Alice <alice.millour@abtela.eu>
2016-10-31 16:02:56 +0100

    adding responsive desing to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,8 @@
 @endsection
 @section('content')
 <div class="fill">
-    <div class="container">
-        <div id="main">
+    <div class="main">
+        <div class="main-container">
             <br><br>
             <div class="flash-message">
                 @foreach (['danger', 'warning', 'success', 'info'] as $msg)
@@ -19,28 +19,33 @@
                 @endif
                 @endforeach
             </div>
+
             <div class="title ostrich">BISAME</div>
             @if(!$game_available)
-            <h3 class="info-message"> Commencez par l'entraînement (quatre phrases) pour débloquer le jeu et commencer à gagner des points : <br>
+            <h3 class="info-message">Commencez par l'entraînement (quatre phrases) pour débloquer le jeu et commencer à gagner des points :
             </h3>
+            <br>
             @else
-            <h3 class="info-message"> Bravo, vous avez débloqué la phase de production d'annotations ! A vous de jouer !<br>
+            <h3 class="info-message">Bravo, vous avez débloqué la phase de production d'annotations ! A vous de jouer !
             </h3>
+            <br>
             @endif
-            <div class="row main-container">
+            <div class="button-wrapper">
                 <span>
-                    <a class='btn btn-default play-button active-button ostrich' href="/home/training">S'entrainer</a>
+                    <a class='btn btn-default play-button active-button ostrich' id="play-button-1" href="/home/training">S'entrainer</a>
                 </span> 
                 @if($game_available)
                 <span>
-                    <a class='btn btn-default play-button active-button ostrich' href="/home/start">Produire des annotations !</a>
+                    <a class='btn btn-default play-button active-button ostrich' id="play-button-2" href="/home/start">Produire des annotations !</a>
                 </span>
                 @else
                 <span>
-                    <a class='btn btn-default play-button b-disabled ostrich' disabled>Produire des annotations !</a>
+                    <a class='btn btn-default play-button b-disabled ostrich' id="play-button-2" disabled>Produire des annotations !</a>
                 </span>
                 @endif
+
             </div>
+
             <h4 class="info-message">
                 <br><b><span class="ostrich">BISAME</span> </b>est une application permettant de recueillir des annotations linguistiques auprès des locuteurs de l'alsacien. </br>
                 <br>Assignez la bonne catégorie grammaticale aux mots proposés, nous construirons des ressources et des outils pour inclure l'alsacien dans les technologies du langage !</br>
@@ -48,20 +53,21 @@
             <h4 class="info-message">
                 <br>Contact : Alice Millour - alice.millour@abtela.eu </br>
             </h4>
-            @yield('content')   
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
-            <BR>&nbsp;<BR>
         </div>
-        <div id="scoreboard" style="float: left;">
-            @include('partials.scoreboard')
-        </div>
-    </div>
+        <div class="main-footer">
+            <div id="scoreboard">
+                @include('partials.scoreboard')
+            </div> 
+        </div> 
+    </div> 
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
+    <BR>&nbsp;<BR>
 </div>
 
 @endsection
