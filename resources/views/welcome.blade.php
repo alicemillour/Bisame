f005570f09f8a2c07d02d238e6a679380398aa31
Alice <alice.millour@abtela.eu>
2016-11-02 17:34:47 +0100

    add link to France Bleu Elsass

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,32 @@
         <span>
             <a class='btn btn-default play-button active-button ostrich' href="/register">Inscription</a>
         </span>
+        @else
+        @if(!$game_available)
+        <h3 class="info-message">Commencez par l'entraînement (quatre phrases) pour débloquer le jeu et commencer à gagner des points :
+        </h3>
+        <br>
+        @else
+        <h3 class="info-message">Bravo, vous avez débloqué la phase de production d'annotations ! A vous de jouer !
+        </h3>
+        <br>
         @endif
-        
+        <div class="button-wrapper">
+            <span>
+                <a class='btn btn-default play-button active-button ostrich' id="play-button-1" href="/home/training">S'entrainer</a>
+            </span> 
+            @if($game_available)
+            <span>
+                <a class='btn btn-default play-button active-button ostrich' id="play-button-2" href="/home/start">Produire des annotations !</a>
+            </span>
+            @else
+            <span>
+                <a class='btn btn-default play-button b-disabled ostrich' id="play-button-2" disabled>Produire des annotations !</a>
+            </span>
+            @endif
+        </div>
+        @endif
+
     </div>
     <div class="info-wrapper">
         <h3 class="info-message">
@@ -23,10 +47,17 @@
             <br> Aucun des outils des nouvelles technologies de la langue - par exemple : correction orthographique, aide à la traduction, extraction d'information - qui contribuent à faire exister les langues sur Internet n'est développé pour l'alsacien.</br> 
             <br> La raison ? Il existe très peu de données "annotées", c'est-à-dire enrichies d'informations linguistiques, à partir desquelles développer de tels outils.
             C'est pourquoi nous faisons appel à vous : locuteurs de l'alsacien, passionnés ou non de grammaire, désireux dans tous les cas de contribuer au déploiement de votre langue, participez grâce à <b><span class="ostrich"> BISAME</span> </b> à la création d'un corpus de l'alsacien annoté en catégories grammaticales !</br> 
-            <br> Pour tout complément d'information sur ce projet de recherche réalisé dans le cadre d'un master en Traitement Automatique des Langues à la Sorbonne en collaboration avec l'équipe du projet RESTAURE du LiLPa de Strasbourg,  <a style="color:red" href="mailto:alice.millour@abtela.eu?Subject=[Bisame]Inscription" style="color:black" target="_top">Me contacter par mail</a>
-                        . </br> Ce projet étant en cours d'amélioration n'hésitez pas à me transmettre vos remarques ou suggestions !<br>
-            <br> Alice </br>
-
+            <br> Pour tout complément d'information sur ce projet de recherche réalisé dans le cadre d'un master en Traitement Automatique des Langues à la Sorbonne en collaboration avec l'équipe du projet RESTAURE du LiLPa de Strasbourg, 
+            <a style="color:#AC1E44;" href="mailto:alice.millour@abtela.eu?Subject=[Bisame]Contact" style="color:black" target="_top">Me contacter par mail</a>
+            . </br> Ce projet étant en cours d'amélioration n'hésitez pas à me transmettre vos remarques ou suggestions !<br>
+            <br> Alice 
+            <br>
+            <a style="color:#AC1E44;" href="https://www.francebleu.fr/emissions/l-alsace-vue-par-le-net-en-alsacien/elsass/l-alsace-vue-par-le-net-en-alsacien-57"
+               target="_blank"> <br> <b> Lien vers la chronique de Pierre Nuss (France Bleu Elsass) </b></a> 
+          <span>
+               <img style="margin:1%" height="42" width="42" src="/images/fbe.png"/>  
+ 
+            </span>
         </h3>
     </div>
 </div>
