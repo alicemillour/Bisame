94589a607852808ccd7ea6381b8246f404baa867
Alice <alice.millour@abtela.eu>
2016-11-09 16:13:28 +0100

    fix toggle dropdown menu navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,4 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<!--<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">-->
-
 <nav class="navbar navbar-default navbar-fixed-top light-background-colored"  id="topnavbar">
     <div class="container-fluid">
         <div class="light-background-colored">
@@ -65,3 +62,6 @@
         </div>
     </div>
 </nav>
+
+
+    
