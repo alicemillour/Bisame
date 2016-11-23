a8938ea37eabc4e5e17a33a8c2617f71087f04c0
Alice <alice.millour@abtela.eu>
2016-11-23 13:47:47 +0100

    correct scores in user table

--- database/seeds/UserTableSeeders.php
+++ database/seeds/UserTableSeeders.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Repositories\AnnotationRepository;
 
 class UserTableSeeder extends Seeder {
+    protected $annotationRepository;
 
+    public function __construct(AnnotationRepository $annotationRepository) {
+        $this->annotationRepository = $annotationRepository;
+    }
 
-    public function run()
-	{
-            // Uncomment the below to wipe the table clean before populating
+    public function run() {
+        // Uncomment the below to wipe the table clean before populating
 //            DB::table('users')->delete();   
 //		for($i = 0; $i < 10; ++$i)
 //		{
@@ -18,12 +22,17 @@ class UserTableSeeder extends Seeder {
 //				'is_admin' => false
 //			]);
 //		}
-            DB::table('users')->insert([
-                            'name' => 'Admin2',
-                            'email' => 'alice.millour2@abtela.eu',
-                            'password' => bcrypt('B!$@m32'),
-                            'is_admin' => true
-            ]);
+        # recalculate confidence_score for all users 
+        $users = DB::table('users')->get();
+        foreach ($users as $user) {
+            $number_annotated_words_on_reference = $this->annotationRepository->get_number_annotations_on_reference($user->id)->count;
+            $number_correct_annotations_on_reference = $this->annotationRepository->get_number_correct_annotations_on_reference($user->id)->count;
+            if ($number_annotated_words_on_reference != 0) {
+                $new_confidence_score = $number_correct_annotations_on_reference / $number_annotated_words_on_reference;
+                debug($new_confidence_score);
+                DB::table('users')->where('users.id', $user->id)->update(['score' => $new_confidence_score]);
+            }
+        }
 //            DB::table('users')->insert([
 //                            'name' => 'Gamer',
 //                            'email' => 'test@gamer.bisame',
@@ -31,5 +40,6 @@ class UserTableSeeder extends Seeder {
 //                            'is_admin' => true,
 //                            'is_in_training' => false
 //            ]);
-	}
-}
+        }
+    }
+    
