afd5ea590043888206e2780d7458edb7b39ca957
Alice <alice.millour@abtela.eu>
2016-11-24 13:38:27 +0100

    add gerard1 texte

---
+++ resources/views/partials/gerard1.blade.php
@@ -0,0 +1,18 @@
+KIND HASCH GEDULD 
+
+Wenn dinna Màma àlt worra ìsch
+Un dü salbscht nìmma gànz jung bìsch,
+Do gìt’s àls a manka schwara Stund,
+Waga dr Màma Ihrem Gedachtnis-Schwund.
+
+Drno brücht Sie dìch gànz notwandig,
+Do müasch Ihra bisteh fàscht standig,
+Denn Sie hàt dìch z’froga un z’froga !
+Blib geduldig, àui wenn’s di düat ploga.
+
+Un wenn Sie s’glicha frogt, ìmmer wìder,
+Beklàg di nìt Kìnd, wìr nìt bìtter,
+Denn as kummt dia Zitt, dank güat drà
+Wu Sie dìch Nix meh froga kà !
+
+                                gK 
