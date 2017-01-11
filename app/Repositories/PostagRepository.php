9d880516f8211396bfce7fa867198a22dff205e6
Alice <alice.millour@abtela.eu>
2017-01-11 14:48:23 +0100

    Restore postag suggestion based on best confidence score

--- app/Repositories/PostagRepository.php
+++ app/Repositories/PostagRepository.php
@@ -70,7 +70,7 @@ class PostagRepository extends ResourceRepository {
                 ->select(DB::raw('postag_id as id, name, full_name, description'))
                 ->distinct()
                 ->where('word_id', $word_id)
-                ->where('confidence_score', '=', '100');
+                ->orderBy('confidence_score', 'desc');
         return $annotations;
     }
 
