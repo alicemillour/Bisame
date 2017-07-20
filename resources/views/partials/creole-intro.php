f5503e62f6eb2b3bf50201ef85b5480f9c97b078
Alice <alice.millour@abtela.eu>
2017-07-20 23:02:32 +0200

    correct intro

--- resources/views/partials/creole-intro.php
+++ resources/views/partials/creole-intro.php
@@ -1,16 +1,16 @@
 <br>
 
-<b> Gwadoupéen Doubout! <br>
+<b style="font-size: 1.3em"> Gwadoupéen Doubout </b>!<b style="font-size: 1.3em">  <br>
 Kréyòl-la sé tan nou, an nou ba-y valè a-y, an nou voyé-y monté.<br><br>
 </b>
 
 Malgré ses 450 000 locuteurs, le <b class="ostrich">Creole Guadeloupeen</b> fait partie des langues peu dotées dans le domaine du traitement automatique du langage. 
 
 <br>Cela s’explique principalement par la faible quantité de données électroniques accessibles en ligne. <br> De plus quand celles-ci sont accessibles, le plus souvent elles ne sont pas annotées en catégorie grammaticale.
- Nous avons donc décidé de développer cette plate-forme afin de récolter des données linguistiques annotées qui permettront de développer des outils informatiques pour le Créole Guadeloupéen.  
+ Nous avons donc décidé de développer cette plate-forme afin de recueillir des données linguistiques annotées qui permettront de développer des outils informatiques pour le Créole Guadeloupéen.  
 
 <br>Créolophones, natifs ou amateurs, nous avons donc besoin de vous ! 
-   <br>Avec <b class="ostrich">KRIK</b>, participez à la création du premier corpus annoté en catégories grammaticales du Créole Guadeloupéen.
+<b>  <br>Avec <b class="ostrich">KRIK</b>, participez à la création du premier corpus annoté en catégories grammaticales du Créole Guadeloupéen. </b>
  
 <br>Des questions, des remarques sur le projet ? N’hésitez pas à  <a href="/contact">nous contacter</a>.
 <br><br>
