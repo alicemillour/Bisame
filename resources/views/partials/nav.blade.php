7db6a1c3b69d46ed235a5d0893912a73460f8d49
Alice <alice.millour@abtela.eu>
2017-08-14 15:41:36 +0200

    test merge depuis master to bisame2 et krik2017 (ajout du profil)

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -10,22 +10,22 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-
+                    
                 <!-- Branding Image -->
                 <a class="navbar-brand ostrich title-app-navbar my-navbar-hover" style="white-space: nowrap;" href="{{ url('/home') }}">
                     <i class="fa fa-home fa-fw" aria-hidden="true"></i><b> Krik </b> 
                 </a>
                 <!--<div class="collapse navbar-collapse" id="app-navbar-collapse">-->
-                    <ul class="nav navbar-nav navbar-right navbar-button-text">
-<!--                        <li><a class="my-navbar-hover navbar-nav nav navbar-button-text" href="{{ url('/textes') }}">
-                                <b> Les textes originaux</b>  !                 </a></li>-->
-                    </ul>
+                <ul class="nav navbar-nav navbar-right navbar-button-text">
+                    <!--                        <li><a class="my-navbar-hover navbar-nav nav navbar-button-text" href="{{ url('/textes') }}">
+                                                    <b> Les textes originaux</b>  !                 </a></li>-->
+                </ul>
                 <!--</div>-->
             </div>
-
+                
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Right Side Of Navbar -->
-
+                    
                 <ul class="nav navbar-nav navbar-right">
                     <!-- <li><a class="my-navbar-hover navbar-nav nav navbar-button-text" href="{{ url('/textes') }}">
                        <b> Les textes originaux</b>  ! 
@@ -52,9 +52,9 @@
                         <a href="#" class="dropdown-toggle my-navbar-hover my-navbar-click" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{$name}}<span class="caret"></span>
                         </a>
-
+                        
                         <ul class="dropdown-menu" role="menu">     
-                            <!--<li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>Mon profil</a></li>-->
+                            <li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>Mon profil</a></li>
                             <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Déconnexion</a></li>
                         </ul>
                     </li>
