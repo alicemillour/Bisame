b109b024b13e94a6abe30771398dde4ba507ce50
Alice <alice.millour@abtela.eu>
2017-08-14 15:49:04 +0200

    indent home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             <span>
                 <a class='btn btn-default play-button active-button ostrich' href="/login">Connexion</a>
             </span>
-
+            
             <span>
                 <a class='btn btn-default play-button active-button ostrich' href="/register">Inscription</a>
             </span>
@@ -49,38 +49,38 @@
             <br>
             @endif
             <div class="info-message">
-
+                
                 <div class="fancy-border" style="background-color:rgba(249, 242, 236, 0.4)">
                     <h4><span class="ostrich alert-message"><u>Nouveau </u> ! <br><br>
                         </span>  Les outils créés grâce aux annotations produites jusqu'à maintenant  <br>
                         facilitent l'annotation en vous proposant une catégorie probable. <br>
                         Validez-les (<img tyle="padding-left: 2px; padding-right: 2px; display:none" src="/images/check.png">)
                         ou corrigez-les (<img tyle="padding-left: 2px; padding-right: 2px; display:none" src="/images/no.png">) dans la phase de production d'annotations ! </h4>
-
+                    
                     <h4>
                         En ce moment, le Vautour est à l'honneur
                         (<a href="https://als.wikipedia.org/wiki/Altweltgeier" style="color:black"  target="_blank" >article Wikipédia</a>) : <br>
                         <!--Il reste : <b> ? phrases à annoter sur {{$total_sentences->count}}, </b>--> 
                         <br> Il reste <b> {{$unannotated_words->count}} mots à annoter</b> (ensemble !). <br><br>
-
+                        
                         <div class="progress" style="width:80%; margin: 0 auto">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="{{$progression}}"
                                  aria-valuemin="0" aria-valuemax="100" style="width:{{$progression}}%">
                             </div>
                         </div>
                         <br>
-
+                        
                         <span class="ostrich alert-message"><u>Nouveaux Corpus</u> : <br> </span><br>
-
-<!--                        Un nouveau texte de Raymond Weissenburger, <i>E Hochzit in de 50er Johre</i> est disponible !<br>
-(Le texte complet est disponible <a href="/textes"  style="color:black"  target="_blank" >ici, rubrique Œuvres Littéraires</a>)
+                        
+                        <!--                        Un nouveau texte de Raymond Weissenburger, <i>E Hochzit in de 50er Johre</i> est disponible !<br>
+                        (Le texte complet est disponible <a href="/textes"  style="color:black"  target="_blank" >ici, rubrique Œuvres Littéraires</a>)
                         -->
                         De nouveaux articles sont disponibles : (<a href="https://als.wikipedia.org/wiki/Tony_Troxler" style="color:black"  target="_blank" >Tony Troxler</a>, 
                         <a href="https://als.wikipedia.org/wiki/Kernkraftwerk_Fessenheim" style="color:black"  target="_blank" >Àtomkràftwark vu Fassena</a> et 
                         <a href="https://als.wikipedia.org/wiki/Delphine_Wespiser" style="color:black"  target="_blank" >Delphine Wespiser</a>)
                         <br>
                         <br> Il reste <b> {{$unannotated_words_Hoch->count}} mots à annoter </b> (ensemble !). <br><br>
-
+                        
                         <div class="progress" style="width:80%; margin: 0 auto">
                             <div class="progress-bar progress-bar-alert" role="progressbar" aria-valuenow="{{$progression_Hoch}}"
                                  aria-valuemin="0" aria-valuemax="100" style="width:{{$progression_Hoch}}%">
@@ -88,14 +88,14 @@
                         </div>
                         <br>
                     </h4>
-
-
+                    
+                    
                 </div>
                 <br>
-
+                
             </div>
-
-
+            
+            
         </div>
         <div class="info-wrapper">
             <h3 class="info-message" style="text-align: center; margin: 0 auto;">
@@ -112,7 +112,7 @@
                    target="_blank"> <br> <b> Lien vers la chronique de Pierre Nuss (France Bleu Elsass)</b></a> 
                 <span>
                     <img style="margin:1%" height="42" width="42" src="/images/fbe.png"/>  
-
+                    
                 </span>
             </h3>
         </div>
