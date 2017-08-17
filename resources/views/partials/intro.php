26b2068bf5ccb76063af4de94a558cd16ae647d9
Alice <alice.millour@abtela.eu>
2017-08-17 14:16:49 +0200

    add right description and weissenburger file for download

--- resources/views/partials/intro.php
+++ resources/views/partials/intro.php
@@ -1,20 +1,15 @@
+<br> L'<b class="ostrich">alsacien</b> fait partie de la grande majorité des "langues peu dotées" au sens des technologies du langage.</br>  
+<br> Aucun des outils des nouvelles technologies de la langue - par exemple : correction orthographique, aide à la traduction, extraction d'information - qui contribuent à faire exister les langues sur Internet n'est développé pour l'alsacien.</br> 
+<br> La raison ? Il existe très peu de données "annotées", c'est-à-dire enrichies d'informations linguistiques, à partir desquelles développer de tels outils.
+C'est pourquoi nous faisons appel à vous : locuteurs de l'alsacien, passionnés ou non de grammaire, désireux dans tous les cas de contribuer au déploiement de votre langue, participez grâce à <b><span class="ostrich"> BISAME</span> </b> à la création d'un corpus de l'alsacien annoté en catégories grammaticales !</br> 
+<br> Pour tout complément d'information sur ce projet de recherche réalisé dans le cadre d'un doctorat en Traitement Automatique des Langues à la Sorbonne en collaboration avec l'équipe du projet RESTAURE du LiLPa de Strasbourg, 
+<a style="color:#AC1E44;" href="mailto:alice.millour@abtela.eu?Subject=[Bisame]Contact" style="color:black" target="_top">Me contacter par mail</a>
+. </br> Ce projet étant en cours d'amélioration n'hésitez pas à me transmettre vos remarques ou suggestions !<br>
+<br> Alice 
 <br>
-
-<b style="font-size: 1.3em"> Gwadoupéen Doubout </b><span style="font-size: 1.3em">!</span><b style="font-size: 1.3em">  <br>
-Kréyòl-la sé tan nou, an nou ba-y valè a-y, an nou voyé-y monté.<br><br>
-</b>
-
-Malgré ses 450 000 locuteurs, le <b class="ostrich">Creole Guadeloupeen</b> fait partie des langues peu dotées dans le domaine du traitement automatique du langage. 
-
-<br>Cela s’explique principalement par la faible quantité de données électroniques accessibles en ligne. <br> Par ailleurs il n'existe pas encore de ressources "annotées", c'est à dire enrichies d'informations linguistiques (par exemple, la catégorie grammaticale des mots).
- Nous avons donc décidé de développer cette plate-forme afin de recueillir des données linguistiques annotées qui permettront de développer des outils informatiques pour le Créole Guadeloupéen.  
-
- <br><b style="font-size: 1.1em">Créolophones, natifs ou amateurs, nous avons donc besoin de vous</b> ! 
- <b>  <br>Avec <b class="ostrich">KRIK</b>, participez <u> ensemble</u> à la création du premier corpus annoté en catégories grammaticales du Créole Guadeloupéen. </b>
- 
-<br>Des questions, des remarques sur le projet ? N’hésitez pas à  <a href="/contact">nous contacter</a>.
-<br><br>
- 
-
-Gwladys & Alice
-<br><br> 
+<a style="color:#AC1E44;" href="https://www.francebleu.fr/emissions/l-alsace-vue-par-le-net-en-alsacien/elsass/l-alsace-vue-par-le-net-en-alsacien-57"
+   target="_blank"> <br> <b> Lien vers la chronique de Pierre Nuss (France Bleu Elsass)</b></a> 
+<span>
+    <img style="margin:1%" height="42" width="42" src="/images/fbe.png"/>  
+                        
+</span>
