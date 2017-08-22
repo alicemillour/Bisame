05e55924297128d1e8a5b312a15f4feb7333efc5
Alice <alice.millour@abtela.eu>
2017-08-22 10:48:24 +0200

    correct image url facebook preview

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta property="og:image" content="url('/images/ppic.jpg')" />
+        <meta property="og:image" content="{{ asset('images/images/ppic.jpg') }}" />
         
         <title>Bisame</title>
 
