98a5e8e032684c13a92889670a6dd4196fe18ab1
Alice <alice.millour@abtela.eu>
2017-08-22 10:21:26 +0200

    add pic for Facebook

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:fb="http://ogp.me/ns/fb#">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta property="og:image" content="url('/images/ppic.jpg')" />
+        
         <title>Bisame</title>
 
         <!-- Fonts -->
