f39eba950c0666777a201ee7ec5735e0ce11eae0
Alice <alice.millour@abtela.eu>
2017-08-22 17:05:13 +0200

    add why

---
+++ resources/views/partials/why.blade.php
@@ -0,0 +1,21 @@
+<h3> Bisame ? En quoi ça consiste ? </h3>
+
+<div class="media">
+        <div class="media-body">
+                <video width="800" height="500" controls>  
+    <source src="{{ asset('video/annotation.mp4') }}" type="video/mp4">  
+    <source src="{{URL::asset('video/annotation.ogv')}}" type="video/ogg">  
+    Your browser does not support the video tag.
+    </video>
+<!--            <iframe width="900" height="500" src="{{ asset('video/annotation.mp4') }}" frameborder="0" allowfullscreen>
+            </iframe>
+             <source src="pr6.ogv" type="video/ogg; codecs=theora,vorbis" />
+  <source src="pr6.mp4" />
+        --></div>
+    </div>
+
+<h3> Pourquoi participer ? <h3/>
+    
+Tandis que les annotations s'accumulent... La taille du corpus annoté augmente ! 
+
+Ce projet a déjà donné lieu à une publication, consultable ici : 
