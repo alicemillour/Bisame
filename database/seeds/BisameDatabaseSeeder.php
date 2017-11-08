177b13f49a9127ffebf0f58a5ab1035bf51f9869
Alice <alice.millour@abtela.eu>
2017-11-08 11:10:38 +0100

    add new seeds

---
+++ database/seeds/BisameDatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Word;
+
+class BisameDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    Model::unguard();
+    //$this->call('UserTableSeeder');
+    $this->call('PostagTableSeeder');
+    $this->call('CorpusTableSeeder');
+    $this->call('WordTableSeeder');
+    $this->call('AnnotationTableSeeder');
+    $this->call('AddFullNameToPostagsSeeder');
+    Model::reguard();
+
+    }
+}
