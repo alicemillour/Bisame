74d853c4558bd3d16c436fc574b840c06579f997
jeanlef <nicolas.lefebvre@inria.fr>
2017-12-19 19:12:58 +0100

    translations

--- resources/lang/krik/game.php
+++ resources/lang/krik/game.php
@@ -2,7 +2,7 @@
 
 return [
 	'app-name' => 'Krik',
-	'text-intro' => 'Aidez la science en annotant des phrases en créole guadeloupéen',
+	'text-intro' => 'Aidez la science en annotant des phrases en créole guadeloupéen.',
 	'message-error' => "Annotation incorrecte... Awa ! ti ni on érrè. Rappel sur les catégories choisies :",
 	'message-success' => "Annotation(s) correcte(s) ! Woulo !",
 ];
