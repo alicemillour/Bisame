ca1727da4915b7ada915a302eab9423013d57737
jeanlef <nicolas.lefebvre@inria.fr>
2018-01-30 15:48:32 +0100

    display of error messages

--- resources/views/components/alerts/dismissible.blade.php
+++ resources/views/components/alerts/dismissible.blade.php
@@ -1,9 +1,13 @@
-<div class="alert alert-{{ $type }} alert-dismissible" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+<div class="row">
+	<div class="col-8 offset-2">
+		<div class="alert alert-{{ $type }} alert-dismissible" role="alert">
+		    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 
-    @if (isset($title))
-        <strong>{{ $title }}</strong>
-    @endif
+		    @if (isset($title))
+		        <strong>{{ $title }}</strong>
+		    @endif
 
-    {{ $slot }}
+		    {{ $slot }}
+		</div>
+	</div>
 </div>
