3e96d6438ed7294a64df357ba6e827f042fc2717
jeanlef <nicolas.lefebvre@inria.fr>
2018-01-31 15:24:57 +0100

    notification when a new recipe is created

---
+++ resources/lang/fr/notifications.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'anecdotes' => 'Une anecdote est ajoutée à une de mes recettes',
+	'discussions' => 'Un commentaire est posté sur une de mes recettes',
+	'alterntive-texts' => 'Une variante est ajoutée à une de mes recettes',
+	'messages' => 'Un message est posté sur une discussion à laquelle j\'ai participé',
+	'photos' => 'Une photo est ajoutée à une de mes recettes',
+	'contact' => 'Recevoir les demandes de contact',
+	'report' => 'Recevoir les signalements pour contenu inapproprié',
+	'all-recipes' => 'Recevoir une notification quand une nouvelle recette est créée',
+	'all-photos' => 'Recevoir une notification quand une photo est ajoutée à une recette',
+];
