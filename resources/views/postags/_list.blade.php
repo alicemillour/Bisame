7978e9994c0304ce591530dbc9aec5537712e6d0
jeanlef <nicolas.lefebvre@inria.fr>
2018-01-31 16:12:52 +0100

    fix help

--- resources/views/postags/_list.blade.php
+++ resources/views/postags/_list.blade.php
@@ -8,19 +8,18 @@
           @endif
           >
           {{ $postag->full_name }} <em>({{ $postag->name }})</em>
-          <button class="btn btn-light float-right help">
+{{--           <button class="btn btn-light float-right help">
           <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="collapse" data-target="#help{{ $postag->id }}" aria-expanded="false"></i>
-          </button>
+          </button> --}}
+          <div class="list-group-item-action collapse" id="help{{ $postag->id }}">
+            {!! $postag->description !!}
+          </div>          
         </div>
       @else
         <div class="postag list-group-item list-group-item-action disabled warning" data-postag-id="{{ $postag->id }}" data-toggle="tooltip" title="Catégorie difficile, faire la formation ?" data-placement="left">
           {{ $postag->full_name }} <em>({{ $postag->name }})</em><i class="float-right fa fa-exclamation-triangle" aria-hidden="true"></i>
         </div>
       @endif
-
-      <div class="list-group-item-action collapse" id="help{{ $postag->id }}">
-        {!! $postag->description !!}
-      </div>
 
     @endif
   @endforeach
