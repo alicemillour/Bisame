2b354a7e974366177b3a5d54b4a0df64570e65df
jeanlef <nicolas.lefebvre@inria.fr>
2018-02-14 17:00:42 +0100

    laurel badges

--- public/js/badges.js
+++ public/js/badges.js
@@ -2,9 +2,9 @@ $(function () {
   if($('#badgeModal').length>0){
     $('#badgeModal').modal('show');
 	$('#badgeModal').on('shown.bs.modal', function (e) {
-	  $('#badgeModal').delay(1500).fadeOut(2500, function(){
-	      $('#badgeModal').modal('hide');
-	  });
+      setTimeout(function() {
+	        $('#badgeModal').modal('hide');
+	    }, 1500);
 	})
   }
 })
