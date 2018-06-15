339f41cf28a17dee961cbcd6d60466a7768e6301
Alice <alice.millour@abtela.eu>
2018-06-15 08:40:48 +0200

    fix recipe with no image center

--- resources/views/recipes/_show-welcome_1.blade.php
+++ resources/views/recipes/_show-welcome_1.blade.php
@@ -26,13 +26,13 @@
     </div>
     @if($recipe->medias->count()>0)
     <div class="row">
-            <div class="col-md-6">
-                
-                
+        <div class="col-md-6">
+            
+            
             <img src="{{ asset($recipe->medias->first()->filename) }}" style="margin-left: auto; 
                  margin-right: auto;
                  display: block;max-height:100%; max-width:100%;" />
-            @endif
+            
         </div>
         <div class="col-md-6">
             
@@ -63,7 +63,34 @@
             </div>
         </div>
     </div>
-    
+    @else 
+        
+    <h4>{{ link_to_route('recipes.show', $recipe->title, $recipe) }}</h4>
+            
+    <p class="card-text">
+        <span class="text-muted">
+            @component('users._avatar', ['user' => $recipe->author])
+                    
+            @endcomponent
+            {{ __('recipes.recipe-by') }}
+            @if($recipe->author->trashed())
+            {{ $recipe->author->name }}
+            @else
+            {{ link_to_route('users.show', $recipe->author->name, $recipe->author) }}
+            @endif			
+        </span>
+    </p>
+            
+    <div class="card-text text-truncate">{{ $recipe->content }}</div>
+    <div class="card-text text-right">{{ link_to_route('recipes.show', "lire la suite...", $recipe) }}</div>
+    <div class="card-text text-right">
+        @if(!$recipe->annotated)
+        <a href="{{ route('recipes.annotations', $recipe) }}"  class="btn annotate-button active-button">Annoter la recette</a>
+        @elseif(!$recipe->validated)
+        <a href="{{ route('recipes.annotations', $recipe) }}"  class="btn validate-button active-button">Valider la recette</a>
+        @endif
+    </div>
+    @endif
 </div>
 <hr/>
 <!--<h4 class="card-subtitle mb-1 text-muted" >{{ link_to_route('recipes.show', $recipe->title, $recipe, ['class' => 'card-link' ]) }}
