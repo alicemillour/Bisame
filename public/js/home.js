5d5a764b37d56db6d34caa6a1e42571de412bd9e
Alice <alice.millour@abtela.eu>
2018-08-30 17:40:32 +0200

    prevent pop up closing

--- public/js/home.js
+++ public/js/home.js
@@ -2,8 +2,8 @@
 var modal = document.getElementById('id01');
 document.getElementById('id01').style.display='block'
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
