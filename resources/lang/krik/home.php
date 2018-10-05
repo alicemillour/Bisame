561b6010384674fdb6cb987e163ebed6e36adcf8
Alice <alice.millour@abtela.eu>
2018-10-05 19:21:50 +0200

    wording

--- resources/lang/krik/home.php
+++ resources/lang/krik/home.php
@@ -5,4 +5,6 @@ return [
 	'message-button-part1' => "Maké sé fraz-la !",
 	'message-button-part2' => "(Produire des annotations)",
         'langue-de' => "du créole guadeloupéen",
+        'variantes-types' => "régionales ou orthographiques",
+
 ];
