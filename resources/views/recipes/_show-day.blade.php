1c14e77bf65dcb8d5927597ae86b825ea9654726
Alice <alice.millour@abtela.eu>
2018-10-10 13:24:41 +0200

    correction _show-day recettes

--- resources/views/recipes/_show-day.blade.php
+++ resources/views/recipes/_show-day.blade.php
@@ -46,7 +46,6 @@
 
 <div class="card-text text-truncate">{{ $recipe->content }}</div>
 <div class="card-text text-right">{{ link_to_route('recipes.show', "lire la suite...", $recipe) }}</div>
-<<<<<<< HEAD
 <!-- Fonctionnalité annotation : décommenter ci-dessous -->
 {{--<div class="card-text text-right">
     @if(!$recipe->annotated)
@@ -54,6 +53,5 @@
     @elseif(!$recipe->validated)
     <a href="{{ route('recipes.annotations', $recipe) }}"  class="btn validate-button active-button">Valider la recette</a>
     @endif
-<<<<<<< HEAD
 </div>--}}
 <!-- Fonctionnalité annotation -->
