3eb1695ad8a23f8d2132eddeac3a67bb1f80264b
Alice <alice.millour@abtela.eu>
2019-01-20 11:07:33 +0100

    add files

---
+++ resources/views/teaser.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<video width="800" style="display: block;margin: 0 auto;" controls>
+  <source src="{{ asset('video/teaser.mp4') }}" type="video/mp4">
+  <source src="{{ asset('video/teaser.ogg') }}" type="video/ogg">
+  Your browser does not support HTML5 video.
+</video>
+    <br>
+    <div style="display: table;margin: 0 auto;">
+        See you in Brussels !
+    </div>
+
+</body> 
+</html>
+
