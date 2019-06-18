a7c36d835fe7f4174cbb49a96430c792d9ea073a
Alice <alice.millour@abtela.eu>
2019-06-18 18:03:29 +0200

    migrations poem

---
+++ database/migrations/2019_06_18_134401_update_poems_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePoemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+             
+        Schema::table('poems', function (Blueprint $table) {
+            $table->date('deleted_at')->nullable();
+            $table->unsignedSmallInteger('annotated')->after('user_id')->default(0);
+            $table->unsignedSmallInteger('validated')->after('user_id')->default(0);
+            DB::statement('ALTER TABLE recipes MODIFY COLUMN title varchar(400)');
+        });    
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
