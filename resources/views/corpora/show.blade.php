729614c2a523a2983262743dff2976f2d9f78436
Alice <alice.millour@abtela.eu>
2019-09-12 10:59:34 +0200

    add variants

--- resources/views/corpora/show.blade.php
+++ resources/views/corpora/show.blade.php
@@ -13,9 +13,9 @@
 
     <div class="main-container semi-transparent fancy-border">     
         <div>
-            <h2>Liens de téléchargement pour les corpus</h2>
+            <h2> Corpus</h2> <br>
             
-            <h3> Corpus MElt (3 357 tokens) : textes présents sur la plateforme (pré-)annotés par MElt entraîné sur un corpus annoté manuellement de 54 phrases  : </h3>
+            <h3> Corpus_MElt (3 357 tokens) : textes présents sur la plateforme (pré-)annotés par MElt entraîné sur un corpus annoté manuellement de 54 phrases  : </h3>
            
     
             (publiés sous licence <a href="https://creativecommons.org/licenses/by-sa/3.0/" 
@@ -26,7 +26,7 @@
             <br>
             
             
-            <h3> Corpus Participants (1 712 tokens) : textes présents sur la plateforme annoté par les participants sur la plateforme : </h3>
+            <h3> Corpus_Participants (1 712 tokens) : textes présents sur la plateforme annoté par les participants sur la plateforme : </h3>
            
     
             (publiés sous licence <a href="https://creativecommons.org/licenses/by-sa/3.0/" 
@@ -35,7 +35,17 @@
             <a href="{{ url('/download_txt/ayo_users_sentences.txt')  }}" target="_blank"> 
                 <b>Télécharger</b> </a>
             <br>
-            
+            <br>
+            <h2> Variantes </h2> <br>  
+            <h3>
+                Liste des variantes orthographiques proposées par les participants sur la plateforme.</h3>
+            (publiés sous licence <a href="https://creativecommons.org/licenses/by-sa/3.0/" 
+                               target=" https://creativecommons.org/licenses/by-sa/3.0/">
+                     Creative Commons Attribution - Partage dans les Mêmes Conditions 3.0 non transposé (CC BY-SA 3.0)</a>).
+            <a href="{{ url('/download_txt/alternative_words.csv')  }}" target="_blank"> 
+                <b>Télécharger</b> </a>
+            <br>
+            </h3>
         </div>
     </div>
 </div>
